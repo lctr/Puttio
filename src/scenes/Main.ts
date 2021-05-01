cb627a088185eac8a05693402a42d8d1e6e48988
Lictor Guzman <lctrgzmn@gmail.com>
2021-04-30 23:51:05 -0500

    dragging; setting up for spring-like controls

--- src/scenes/Main.ts
+++ src/scenes/Main.ts
@@ -1,17 +1,14 @@
-import { MouseConstraint } from 'matter';
-import Phaser, { CANVAS } from 'phaser';
+import Phaser from 'phaser';
 
-type Scene = Phaser.Scene;
-type ShapeArgs = (number | boolean)[];
-
-enum BallSize {
-  Normal = 16,
+enum BallState {
+  Free, DragStart, Dragging, DragEnd
 }
-
-let world;
 
 export default class Main extends Phaser.Scene {
   ball!: Phaser.Physics.Matter.Image;
+  ballVect!: MatterJS.BodyType;
+  clickState!: BallState;
+  vel: [number, number] = [4, 1];
   constructor () {
     super('Main');
   }
@@ -22,20 +19,47 @@ export default class Main extends Phaser.Scene {
     });
   }
   create () {
+    // set the bounds for which physics applies
     this.matter.world.setBounds(100, 100, 300, 400, 10, true, true, true, true).disableGravity();
     
+    // using static image for simplicity's sake atm
     this.ball = this.matter.add.image(200, 200, 'ball');
     // @ts-ignore
     this.ball.setCircle();
     this.ball.setFriction(0.15);
     this.ball.setBounce(0.8);
-    this.ball.setVelocity(4, 1);
+    this.ball.setVelocity(...this.vel);
+    
+    this.ball.on('mousedown', this.mouseDown);
+    this.ball.on('mouseup', this.mouseUp);
+
+    this.matter.add.mouseSpring();
+  }
+  mouseDown () {
+    
+    this.clickState = BallState.DragStart;
+
+    let { x, y } = this.ball.body.position;
+    this.ballVect = this.matter.add.rectangle(x, y, 10, 5);
+  }
+  mouseUp () {
     
   }
+  dPotential () {
+    this.vel[ 0 ] = this.input.activePointer.getDistanceX();
+    console.log(this.vel);
+  }
   update () {
-    
+    switch (this.clickState) {
+      case BallState.DragStart:
+      case BallState.Dragging:
+      case BallState.DragEnd:
+        this.clickState = BallState.Free;
+      default:
+    }
   }
   render () {
     
   }
+
 }
